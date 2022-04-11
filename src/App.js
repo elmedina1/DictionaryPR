595ef968ccb94a3c06e0b1f182b4029c6be6ccb2
elmedina1 <elmedina.sunje@gmail.com>
2022-04-11 23:30:16 +0200

    added DictionarySearch component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import logo from './foxlogo.png';
+import DictionarySearch from './DictionarySearch';
 
 function App() {
   return (
     <div className="App">
       <div className="header-logo">
-        <img src={logo} alt="fox-logo"/>
+        <img src={logo} alt="fox-logo" />
       </div>
-      
+      <main>
+        <DictionarySearch />
+      </main>
     </div>
   );
 }
---
+++ src/DictionarySearch.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function DictionarySearch() {
+  let [keyword, saveKeyword] = useState(null);
+
+  function searchKeyword(event) {
+    event.preventDefault();
+    alert('searching');
+  }
+
+  function saveSearchKeyword(event) {
+    saveKeyword(event.target.value);
+  }
+
+  return (
+    <div className="DictionarySearch">
+      <form onSubmit={searchKeyword}>
+        <input type="search" onFocus={true} onChange={saveSearchKeyword} />
+      </form>
+    </div>
+  );
+}
