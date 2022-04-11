09e454091fbcc0520d0d51d4ec34c2db4468c495
elmedina1 <elmedina.sunje@gmail.com>
2022-04-11 23:04:56 +0200

    logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import logo from './foxlogo.png';
 
 function App() {
   return (
     <div className="App">
+      <div className="header-logo">
+        <img src={logo} alt="fox-logo"/>
+      </div>
       
     </div>
   );
