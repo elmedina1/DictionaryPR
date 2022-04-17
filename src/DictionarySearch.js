0e9d329d01870fc26b33d48287b0427b8cdd8d6b
elmedina1 <elmedina.sunje@gmail.com>
2022-04-17 19:14:06 +0200

    updated

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function DictionarySearch() {
-  let [keyword, saveKeyword] = useState(null);
+  let [keyword, saveKeyword] = useState("");
 
-  function searchKeyword(event) {
+    function handleResponse(response){
+    console.log(response);
+}
+
+function searchKeyword(event) {
     event.preventDefault();
-    alert('searching');
+  
+    let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log("api yrl"+apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
+
+  //
 
   function saveSearchKeyword(event) {
     saveKeyword(event.target.value);
-    alert(keyword);
+  console.log("riject", keyword);
+  
   }
 
   return (
     <div className="DictionarySearch">
       <form onSubmit={searchKeyword}>
-        <input type="search" onFocus={true} onChange={saveSearchKeyword} />
+        <input type="search" onChange={saveSearchKeyword} />
       </form>
     </div>
   );
